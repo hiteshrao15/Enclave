db7601e9e00558cfb42445079af180bb5d7eea04
Hitesh <143160062+hiteshrao15@users.noreply.github.com>
2026-07-05 18:07:16 +0530

    update

--- client/src/services/contact.service.js
+++ client/src/services/contact.service.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const apiBaseUrl =
+  import.meta.env.VITE_API_BASE_URL ||
+  (import.meta.env.DEV ? "http://localhost:8888/api" : "/api");
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL,
+  baseURL: apiBaseUrl,
 
   headers: {
     "Content-Type": "application/json",
--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import mongoose from "mongoose";
 import logger from "../utils/logger.js";
 
